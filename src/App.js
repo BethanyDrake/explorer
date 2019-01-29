d884cd36c335cbd490e85ac6d272f06feb28c243
Bethany Drake <bethanyd@thoughtworks.com>
2019-01-29 16:02:52 +1100

    2 3x3 maps with wall or path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+
 const Header = styled.header`
   font-size: 3em;
 `;
@@ -11,6 +12,17 @@ const Square = styled.div`
   background: grey;
 `;
 
+const WallSquare = styled.div`
+  width: 12em;
+  height: 12em;
+  background: grey;
+`;
+
+const PathSquare = styled.div`
+  width: 12em;
+  height: 12em;
+  background: #604700;
+`;
 const MapBox = styled.div`
   width: 38em;
   height: 38em;
@@ -22,8 +34,11 @@ const MapBox = styled.div`
 `;
 
 
+const map1 = [["W", "W","W"], ["P","P","P"] , ["P","W","P"]]
+const map2 = [["W", "W","P"], ["P","P","P"] , ["P","W","W"]]
 class App extends Component {
   render() {
+    let map = (Math.random() < 0.5) ? map1 : map2
     return (
       <div className="App">
         <link rel="icon" type="image/svg" href="./logo.svg" sizes="16x16" />
@@ -33,9 +48,7 @@ class App extends Component {
         {/* <img src ={ require('./logo.svg') } /> */}
         <p>(There's not much to explore).</p>
         <MapBox>
-        <Square/><Square/><Square/>
-        <Square/><Square/><Square/>
-        <Square/><Square/><Square/>
+          {map.flat().map((letter, index) => letter === "W" ? <WallSquare key = {index}/> : <PathSquare key = {index}/>)}
         </MapBox>
       </div>
     );
