a0810462a1e144047ab9dd83dc9fedd990475f93
Bethany Drake <bethanyd@thoughtworks.com>
2019-01-29 14:49:05 +1100

    basic hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  font-size: 3em;
+`;
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header className="App-header">
+          Hello! This is an exploration game!
+        </Header>
+        {/* <img src ={ require('./logo.svg') } /> */}
+        <p>(There's not much to explore).</p>
       </div>
     );
   }
