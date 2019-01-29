ccb1543df5dc60c8d875c1187497fb846d8525ff
Bethany Drake <bethanyd@thoughtworks.com>
2019-01-29 16:28:46 +1100

    chicken in center block

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
+import chickenFlying from './img/chicken-pngs/flying/frame-1.png';
 
 const Header = styled.header`
   font-size: 3em;
@@ -23,6 +23,12 @@ const PathSquare = styled.div`
   height: 12em;
   background: #604700;
 `;
+
+const Image = styled.img`
+  width: 12em;
+  height: 12em;
+  background: #604700;
+`;
 const MapBox = styled.div`
   width: 38em;
   height: 38em;
@@ -37,8 +43,11 @@ const MapBox = styled.div`
 const map1 = [["W", "W","W"], ["P","P","P"] , ["P","W","P"]]
 const map2 = [["W", "W","P"], ["P","P","P"] , ["P","W","W"]]
 class App extends Component {
+
   render() {
     let map = (Math.random() < 0.5) ? map1 : map2
+    let squares = map.flat().map((letter, index) => letter === "W" ? <WallSquare key = {index}/> : <PathSquare key = {index}/>)
+    squares[4] = <PathSquare key = "center" > <Image alt="character" src={chickenFlying}/></PathSquare>
     return (
       <div className="App">
         <link rel="icon" type="image/svg" href="./logo.svg" sizes="16x16" />
@@ -48,7 +57,7 @@ class App extends Component {
         {/* <img src ={ require('./logo.svg') } /> */}
         <p>(There's not much to explore).</p>
         <MapBox>
-          {map.flat().map((letter, index) => letter === "W" ? <WallSquare key = {index}/> : <PathSquare key = {index}/>)}
+        {squares}
         </MapBox>
       </div>
     );
